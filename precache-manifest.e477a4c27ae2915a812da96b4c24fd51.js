0f1ffe5ec45c50beaf3b1fb63d21ab41ae6fd906
unknown <andersondenise406@gmail.com>
2019-04-11 20:30:20 -0600

    Updates

---
+++ precache-manifest.e477a4c27ae2915a812da96b4c24fd51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03b7635581b47ba5cf1e",
+    "url": "/deployedclicky/static/js/runtime~main.db7270a9.js"
+  },
+  {
+    "revision": "70cab99968e83d1c2bb8",
+    "url": "/deployedclicky/static/js/main.80044c1e.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/deployedclicky/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "70cab99968e83d1c2bb8",
+    "url": "/deployedclicky/static/css/main.11101a47.chunk.css"
+  },
+  {
+    "revision": "ed85672c295646fa192710238e6e6b87",
+    "url": "/deployedclicky/index.html"
+  }
+];
